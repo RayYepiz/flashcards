b2da6d8a8eca1d06b92baa9b96398416c6a338f8
Raymond Yepiz <93514024+RayYepiz@users.noreply.github.com>
2024-09-08 14:56:04 -0700

    added some more css, specifically a container for the flashcards to make the website more responsive. Used grid template.
    Added scripting to update state of front or back, useRef to always keep a record of our flash card.
    
    Installed Axios and used useEffect to create an API with Open Trivia Database to import some random questions as a place holder.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,39 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import FlashcardList from "./FlashcardList";
 import './app.css'
+import axios from "axios";
 
 function App() {
   const [flashcards, setFlashcards] = useState(SAMPLE_FLASHCARDS);
+
+  useEffect(()=>{
+    axios
+    .get('https://opentdb.com/api.php?amount=10')
+    .then(res => {setFlashcards(
+      res.data.results.map((questionItem, index)=>{
+        const answer = decodeString(questionItem.correct_answer);
+        const options = [
+          ...questionItem.incorrect_answers.map(a=> decodeString(a)), 
+          answer
+        ];
+        return {
+          id:`${index}-${Date.now()}`,
+          question: decodeString(questionItem.question),
+          answer: questionItem.correct_answer,
+          options: options.sort(()=> Math.random() - .5)//adding the .5 will randomize and return 50% of time a positive and the other half a negative number
+        }
+      })
+    )})
+  },[])
+
+  function decodeString(str){
+    const textArea = document.createElement('textarea')
+    textArea.innerHTML=str
+    return textArea.value;
+  }
+
   return (
-    <div className="App">
+    <div className="container">
       <FlashcardList flashcards = {flashcards}/>
     </div>
   );
